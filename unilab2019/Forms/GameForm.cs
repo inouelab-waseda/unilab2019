d139479105927e607dcc46ca3941acb34aa5de1b
やすだ <46004932+sirokome19@users.noreply.github.com>
2019-08-01 17:22:35 +0900

    Revert "usingを追加"

--- unilab2019/Forms/GameForm.cs
+++ unilab2019/Forms/GameForm.cs
@@ -6,18 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
 using System.Windows.Forms;
-using Unilab2019.Objects;
-using Unilab2019.Objects.Enemies;
-using Unilab2019.Objects.FieldObjects;
-using Unilab2019.Objects.Players;
-using Unilab2019.Objects.Goal;
-using Unilab2019.Fields;
-using System.IO;
-using Newtonsoft.Json;
-using System.Collections;
-using Unilab2019.Helpers;
 
 namespace unilab2019.Forms
 {
@@ -63,37 +52,5 @@ namespace unilab2019.Forms
         {
 
         }
-        #region JSONファイルの読み出し
-
-        //何をやってるか不明？？　
-        private void SaveFieldJson(Field field, string fileName)
-        {
-            var filePath = Environment.CurrentDirectory + @"\" + fileName + ".json";
-            var enc = Encoding.GetEncoding("utf-8");
-
-            var output = JsonConvert.SerializeObject(field, Formatting.Indented, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Auto
-            });
-
-            File.WriteAllText(filePath, output, enc);
-        }
-
-        //jsonファイルの読み出し
-        private Field ReadFieldJson(string name)
-        {
-            //現在のコードを実行しているAssemblyを取得
-            var myAssembly = Assembly.GetExecutingAssembly();
-            var sr = new StreamReader(
-                myAssembly.GetManifestResourceStream("Unilab2019.Fields." + name + ".json"),
-                    Encoding.GetEncoding("utf-8"));
-            var input = sr.ReadToEnd();
-            sr.Close();
-
-            var deserialized = JsonConvert.DeserializeObject<Field>(
-                input, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
-            return deserialized;
-        }
-        #endregion
     }
 }
