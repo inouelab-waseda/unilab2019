2ccf3a731c38683f487238a1a3ab17155611ea55
Awakasugi <46044262+Awakasugi@users.noreply.github.com>
2019-08-01 19:04:18 +0900

    jsonファイル読み込み
    ステージの読み込みとinitialize

--- unilab2019/Forms/selectstage.cs
+++ unilab2019/Forms/selectstage.cs
@@ -6,17 +6,51 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
+using Unilab2019.Helpers;
+
+
+
 
 namespace unilab2019.Forms
 {
     public partial class Selectstage : Form
     {
-        GameForm gf1;
+        private Field _field;
+        GameForm gameform;
+
         public Selectstage(GameForm gf)
         {
             InitializeComponent();
-            gf1 = gf;
+            gameform = gf;
+        }
+
+        private Field ReadFieldJson(string name)
+        {
+            //現在のコードを実行しているAssemblyを取得
+            var myAssembly = Assembly.GetExecutingAssembly();
+            var sr = new StreamReader(
+                myAssembly.GetManifestResourceStream("Unilab2019.Fields." + name + ".json"),
+                    Encoding.GetEncoding("utf-8"));
+            var input = sr.ReadToEnd();
+            sr.Close();
+
+            var deserialized = JsonConvert.DeserializeObject<Field>(
+                input, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+            return deserialized;
+        }
+
+        public void _initialize(string fieldName)
+        {
+            _field = ReadFieldJson($"{fieldName}");
+            foreach (var obj in _field.GameObjectList())
+            {
+                if (obj != null && !obj.CanMove) obj.Draw();
+            }
 
         }
 
@@ -69,70 +103,80 @@ namespace unilab2019.Forms
         {
 
             this.Hide();
-            gf1.Show();
+            //_initialize("stage6");
+            gameform.Show();
         }
         //ステージ6のボタン
 
         private void pictureBox5_Click_1(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage5");
+            gameform.Show();
         }
         //ステージ5のボタン
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage1");
+            gameform.Show();
         }
         //ステージ1のボタン
 
         private void PictureBox2_Click_1(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage2");
+            gameform.Show();
         }
         //ステージ2のボタン
 
         private void PictureBox3_Click_1(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage3");
+            gameform.Show();
         }
         //ステージ3のボタン
 
         private void PictureBox4_Click_1(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage4");
+            gameform.Show();
         }
         //ステージ4のボタン
 
         private void PictureBox7_Click(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage7");
+            gameform.Show();
         }
         //ステージ7のボタン
 
         private void PictureBox8_Click(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage8");
+            gameform.Show();
         }
         //ステージ8のボタン
 
         private void PictureBox9_Click(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage9");
+            gameform.Show();
         }
         //ステージ9のボタン
 
         private void PictureBox10_Click(object sender, EventArgs e)
         {
             this.Hide();
-            gf1.Show();
+            //_initialize("stage10");
+            gameform.Show();
         }
         //ステージ10のボタン
     }
