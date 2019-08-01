e620e0ea868e742cc9c6e26f33617246508e11eb
yasuda <sirokome19@yahoo.co.jp>
2019-08-01 17:50:57 +0900

    修正Reflection

--- unilab2019/Forms/GameForm.cs
+++ unilab2019/Forms/GameForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 using Unilab2019.Objects;
 using Unilab2019.Objects.Enemies;
