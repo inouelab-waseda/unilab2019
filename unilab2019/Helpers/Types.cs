ee2e6bff0f2ea2a2c022187d0c4a5c13412eac65
kyamada101 <45187627+kyamada101@users.noreply.github.com>
2019-08-01 19:10:30 +0900

    変数名修正
    indentとrepeat_num

--- unilab2019/Helpers/Types.cs
+++ unilab2019/Helpers/Types.cs
@@ -17,13 +17,18 @@ namespace Unilab2019.Helpers
         /// </summary>
         public Types.Instruction Instruction { get; set; }
 
+        /// <summary>
+        /// forの場合の繰りかえし回数
+        /// </summary>
+        public int Repeat_num { get; set; }
+
         public Types.Direction Diriection { get; set; }
         /// <summary>
         /// Wall,Enemy,Road
         /// </summary>
         public Types.Obj Obj { get; set; }
 
-        public int Indent;
+        public int Indent { get; set; }
 
     }
 }
