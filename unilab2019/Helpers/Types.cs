a1e44a2d76ee651bb91f2ec8f62704d119f33fab
yasuda <sirokome19@yahoo.co.jp>
2019-08-01 18:51:59 +0900

    メンバ変数翻訳

--- unilab2019/Helpers/Types.cs
+++ unilab2019/Helpers/Types.cs
@@ -12,9 +12,15 @@ namespace Unilab2019.Helpers
         public enum Obj { Wall, Enemy, Road }
     }
     public class Code {
+        /// <summary>
+        /// if,for,while
+        /// </summary>
         public Types.Instruction Instruction { get; set; }
 
         public Types.Direction Diriection { get; set; }
+        /// <summary>
+        /// Wall,Enemy,Road
+        /// </summary>
         public Types.Obj Obj { get; set; }
 
         public int Indent;
