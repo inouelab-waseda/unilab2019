d1ca0245006d428ce1e7a9f9810ba161609c02ce
yasuda <sirokome19@yahoo.co.jp>
2019-08-02 18:28:23 +0900

    Types change

--- unilab2019/Helpers/Types.cs
+++ unilab2019/Helpers/Types.cs
@@ -7,28 +7,28 @@ namespace Unilab2019.Helpers
 {
     public static class Types
     {
-        public enum Instruction { IfCode, ForCode, WhileCode, Forward, TurnRight, TurnLeft, End, Stop}
+        public enum Instruction { IfCode, ForCode, WhileCode, Forward, TurnRight, TurnLeft, End, Stop, None}
         public enum Direction { None, Up, Down, Right, Left, Forward, Backward }
-        public enum Obj { Wall, Enemy, Road }
+        public enum Obj { Wall, Enemy, Road ,None}
     }
     public class Code {
         /// <summary>
         /// if,for,while
         /// </summary>
-        public Types.Instruction Instruction { get; set; }
+        public Types.Instruction Instruction { get; set; } = Types.Instruction.None;
 
         /// <summary>
         /// forの場合の繰りかえし回数
         /// </summary>
-        public int Repeat_num { get; set; }
+        public int Repeat_num { get; set; } = 0;
 
-        public Types.Direction Direction { get; set; }
+        public Types.Direction Direction { get; set; } = Types.Direction.None;
         /// <summary>
         /// Wall,Enemy,Road
         /// </summary>
-        public Types.Obj Obj { get; set; }
+        public Types.Obj Obj { get; set; } = Types.Obj.None;
 
-        public int Indent { get; set; }
+        public int Indent { get; set; } = 0;
 
     }
 }
